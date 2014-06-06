ddff56d8fa5e18506a33a38e62eb144e61f100fc
nadia <kanouhimehime@kanouhimehimematoMacBook-Pro.local>
2014-06-06 21:37:11 +0800

    Initial Commit

---
+++ SwiftFirstTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftFirstTest
+//
+//  Created by nadia on 14-6-6.
+//  Copyright (c) 2014年 叶媛媛. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
